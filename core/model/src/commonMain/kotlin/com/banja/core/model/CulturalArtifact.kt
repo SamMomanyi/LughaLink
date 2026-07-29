e2cdc56cdd39dbede966734cd594762bac705459
Samson Momanyi <samsonmomanyi2005@gmail.com>
2026-07-29 23:14:09 +0300

    artifact model

---
+++ core/model/src/commonMain/kotlin/com/banja/core/model/CulturalArtifact.kt
@@ -0,0 +1,9 @@
+package com.banja.core.model
+
+data class CulturalArtifact(
+    val id: String,
+    val title: String,
+    val historyDescription: String,
+    val imageUrl: String,
+    val originRegion: String
+)
