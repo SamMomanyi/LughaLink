dbd97e73df513c2819a5204730926f219eb824cd
Samson Momanyi <samsonmomanyi2005@gmail.com>
2026-07-29 23:12:20 +0300

    language model

---
+++ core/model/src/commonMain/kotlin/com/banja/core/model/Language.kt
@@ -0,0 +1,9 @@
+package com.banja.core.model
+
+data class Language(
+    val id: String,
+    val name: String,
+    val greeting: String,
+    val description: String,
+    val regionCode: String
+)
